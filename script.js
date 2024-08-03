1ccddc7a251671b74378210603fd3cda8aa10e0d
MIKNSJ <sculetoncode@gmail.com>
2024-08-02 22:40:15 -0700

    removed newline

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function clickedButton(key) {
     var output = document.getElementById("calc_output_text");
     
     num+=key.innerText;
-    output.innerText = num.trim();
+    output.innerText = num.replace("\n", "");
 }
