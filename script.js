0211abe244754f1ba6d00633cfaefa5b44cf9ac1
MIKNSJ <sculetoncode@gmail.com>
2024-08-02 22:00:14 -0700

    5

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-var num = "0";
+var num = "";
 var input = "";
-var enableDecimal = false;
-var enableAbsoluteZero = false;
-var enableOperator = false;
-var keepResult = false;
 
 function clickedButton(key) {
     var output = document.getElementById("calc_output_text");
