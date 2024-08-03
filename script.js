151d4886ecfa786dc9cc83d7c01d79b4e9b28bcb
MIKNSJ <sculetoncode@gmail.com>
2024-08-02 21:58:01 -0700

    4

--- script.js
+++ script.js
@@ -8,20 +8,7 @@ var keepResult = false;
 function clickedButton(key) {
     var output = document.getElementById("calc_output_text");
     var key_clear = document.getElementById("key_clear");
-
-    if (key == key_clear) {
-        num = "0";
-        input = "";
-        output.innerText = "0";
-        key_clear.innerText = "AC";
-        enableDecimal = false;
-        enableAbsoluteZero = false;
-        enableOperator = false;
-        keepResult = false;
-    }
     
-    if (key.classList.contains("number")) {
-        num+=key.innerText;
-        output.innerText = num;
-    }
+    num+=key.innerText;
+    output.innerText = num;
 }
