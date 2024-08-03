20c6a126f1cf10d1611260837baa400cec83c7d8
MIKNSJ <sculetoncode@gmail.com>
2024-08-02 22:37:06 -0700

    trim mode

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ var input = "";
 
 function clickedButton(key) {
     var output = document.getElementById("calc_output_text");
-    var key_clear = document.getElementById("key_clear");
     
     num+=key.innerText;
-    output.innerText = num;
+    output.innerText = num.trim();
 }
